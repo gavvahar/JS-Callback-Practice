8a7d87e9793eb827a51c6d8fde67df34a9db332f
gavvahar <nihargavva@gmail.com>
2022-06-21 14:24:42 -0400

    changed the paths

--- assets/JS/index.js
+++ assets/JS/index.js
@@ -1,15 +1,14 @@
-const inventory = newInventory()
-move(inventory).to(0, 0)
+const inventory = newInventory();
+move(inventory).to(0, 0);
 
-const character = newImage('assets/green-character/static.gif')
-move(character).to(100, 250)
+const character = newImage("assets/GIF/green-character/static.gif");
+move(character).to(100, 250);
 
-
-move(newImage('assets/tree.png')).to(200, 450)
-move(newImage('assets/pillar.png')).to(350, 250)
-move(newImage('assets/pine-tree.png')).to(450, 350)
-move(newImage('assets/crate.png')).to(150, 350)
-move(newImage('assets/well.png')).to(500, 575)
-move(newItem('assets/sword.png')).to(500, 555)
-move(newItem('assets/shield.png')).to(165, 335)
-move(newItem('assets/staff.png')).to(600, 250)
+move(newImage("assets/PNG/tree.png")).to(200, 450);
+move(newImage("assets/PNG/pillar.png")).to(350, 250);
+move(newImage("assets/PNG/pine-tree.png")).to(450, 350);
+move(newImage("assets/PNG/crate.png")).to(150, 350);
+move(newImage("assets/PNG/well.png")).to(500, 575);
+move(newItem("assets/PNG/sword.png")).to(500, 555);
+move(newItem("assets/PNG/shield.png")).to(165, 335);
+move(newItem("assets/PNG/staff.png")).to(600, 250);
