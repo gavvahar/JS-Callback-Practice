2234d65ba30214861ec3baf39af2dc7f56785490
gavvahar <nihargavva@gmail.com>
2022-06-21 15:50:35 -0400

    finished with step 4

--- assets/JS/index.js
+++ assets/JS/index.js
@@ -2,52 +2,8 @@ const inventory = newInventory();
 move(inventory).to(0, 0);
 
 const character = newImage("assets/GIF/green-character/static.gif");
-let direction = null;
-let x = 100;
-let y = 250;
 
-document.addEventListener("keydown", function (e) {
-  if (e.repeat) return;
-  switch (e.key) {
-    case "ArrowLeft":
-      direction = "west";
-      break;
-    case "ArrowUp":
-      direction = "north";
-      break;
-    case "ArrowRight":
-      direction = "east";
-      break;
-    case "ArrowDown":
-      direction = "south";
-      break;
-  }
-});
-
-document.addEventListener("keyup", function (e) {
-  direction = null;
-});
-
-setInterval(function () {
-  switch (direction) {
-    case "west":
-      x = x - 1;
-      break;
-    case "north":
-      y = y + 1;
-      break;
-    case "east":
-      x = x + 1;
-      break;
-    case "south":
-      y = y - 1;
-      break;
-  }
-  character.style.left = x + "px";
-  character.style.bottom = y + "px";
-}, 1);
-
-move(character).to(100, 250);
+move(character).withArrowKeys(100, 250);
 
 move(newImage("assets/PNG/tree.png")).to(200, 450);
 move(newImage("assets/PNG/pillar.png")).to(350, 250);
--- assets/JS/move.js
+++ assets/JS/move.js
@@ -1,12 +1,65 @@
 function move(element) {
-    element.style.position = 'fixed'
+  element.style.position = "fixed";
 
-    function moveToCoordinates(left, bottom) {
-        element.style.left = left + 'px'
-        element.style.bottom = bottom + 'px'
+  function moveToCoordinates(left, bottom) {
+    element.style.left = left + "px";
+    element.style.bottom = bottom + "px";
+  }
+
+  function moveWithArrowKeys(left, bottom) {
+    let direction = null;
+    let x = left;
+    let y = bottom;
+
+    character.style.left = x + "px";
+    character.style.bottom = y + "px";
+
+    function moveCharacter() {
+      switch (direction) {
+        case "west":
+          x = x - 1;
+          break;
+        case "north":
+          y = y + 1;
+          break;
+        case "east":
+          x = x + 1;
+          break;
+        case "south":
+          y = y - 1;
+          break;
+      }
+      character.style.left = x + "px";
+      character.style.bottom = y + "px";
     }
 
-    return {
-        to: moveToCoordinates
-    }
+    setInterval(moveCharacter, 1);
+
+    document.addEventListener("keydown", function (e) {
+      if (e.repeat) return;
+      switch (e.key) {
+        case "ArrowLeft":
+          direction = "west";
+          break;
+        case "ArrowUp":
+          direction = "north";
+          break;
+        case "ArrowRight":
+          direction = "east";
+          break;
+        case "ArrowDown":
+          direction = "south";
+          break;
+      }
+    });
+
+    document.addEventListener("keyup", function (e) {
+      direction = null;
+    });
+  }
+
+  return {
+    to: moveToCoordinates,
+    withArrowKeys: moveWithArrowKeys,
+  };
 }
